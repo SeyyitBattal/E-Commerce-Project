fee74e51f3f8df61ae2f2f6d183034229b6f8c2d
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-24 21:11:27 +0300

    Product card map added code

--- e-commerce/src/Datas/Hdata.js
+++ e-commerce/src/Datas/Hdata.js
@@ -13,6 +13,12 @@ import sliderPhoto1 from "../IconsAndPhotos/sliderPhoto1.jpg";
 import facebookFooter from "../IconsAndPhotos/facebookFooter.svg";
 import instagramFooter from "../IconsAndPhotos/instagramFooter.svg";
 import twitterFooter from "../IconsAndPhotos/twitterFooter.svg";
+import bestseller1 from "../IconsAndPhotos/bestseller1.png";
+import bestseller2 from "../IconsAndPhotos/bestseller2.png";
+import blueCircle from "../IconsAndPhotos/blueCircle.svg";
+import greenCircle from "../IconsAndPhotos/greenCircle.svg";
+import orangeCircle from "../IconsAndPhotos/orangeCircle.svg";
+import blackCircle from "../IconsAndPhotos/blackCircle.svg";
 
 export const Hdata = {
   headerArea: {
@@ -30,6 +36,104 @@ export const Hdata = {
   },
   bodyArea: {
     sliderPhoto1,
+    productCarsdArray: [
+      {
+        id: "1",
+        img: bestseller1,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+      {
+        id: "2",
+        img: bestseller2,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+      {
+        id: "1",
+        img: bestseller1,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+      {
+        id: "2",
+        img: bestseller2,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+      {
+        id: "1",
+        img: bestseller1,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+      {
+        id: "2",
+        img: bestseller2,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+      {
+        id: "1",
+        img: bestseller1,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+      {
+        id: "2",
+        img: bestseller2,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+        color1: blueCircle,
+        color2: greenCircle,
+        color3: orangeCircle,
+        color4: blackCircle,
+      },
+    ],
   },
   footerArea: {
     facebookFooter,
--- e-commerce/src/layouts/PageBody.js
+++ e-commerce/src/layouts/PageBody.js
@@ -10,7 +10,28 @@ export const PageBody = () => {
       <p>Featured Products</p>
       <p>BESTSELLER PRODUCTS</p>
       <p>Problems trying to resolve the conflict between</p>
-      BURAYA 6 TANE ÜRÜN KARTI YEŞİL ALAN SOLDA FOTO SAĞDA YAZI
+      ********
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        {Hdata.bodyArea.productCarsdArray.map((productCard, index) => {
+          <div key={index}>
+            <a href="#">
+              <img class="rounded-t-lg" src={productCard.img} alt="" />
+            </a>
+            <p>{productCard.tittle}</p>
+            <p>{productCard.descripton}</p>
+            <div>
+              <p>{productCard.price1}</p>
+              <p>{productCard.price2}</p>
+            </div>
+            <div>
+              <p>{productCard.color1}</p>
+              <p>{productCard.color2}</p>
+              <p>{productCard.color3}</p>
+              <p>{productCard.color4}</p>
+            </div>
+          </div>;
+        })}
+      </div>
       <p>Practice Advice</p>
       <p>Featured Posts</p>
       <p>Problems trying to resolve the conflict between</p>
