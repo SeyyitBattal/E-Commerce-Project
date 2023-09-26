e8761313a47b894a8206dc88e22861b72817e788
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-26 15:48:35 +0300

    Blue header section done

--- e-commerce/src/layouts/Header.js
+++ e-commerce/src/layouts/Header.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { Hdata } from "../Datas/Hdata";
-import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div>
-      <div className="section1 bg-slate-800 flex">
-        <img src={Hdata.headerArea.phoneLogo} />
-        <p>(225) 555-0118</p>
-        <img src={Hdata.headerArea.mailLogo} />
-        <p>battal.arvas@gmail.com</p>
+      <div className="section1 py-4 px-9 font-bold text-white bg-slate-800 flex justify-between ">
+        <div className="flex">
+          <div className="flex">
+            <img src={Hdata.headerArea.phoneLogo} className="pr-1" />
+            <p>(225) 555-0118</p>
+          </div>
+          <div className="flex pl-7">
+            <img src={Hdata.headerArea.mailLogo} className="pr-1" />
+            <p>battal.arvas@gmail.com</p>
+          </div>
+        </div>
+
         <p>Follow Us and get a chance to win 80% off</p>
-        <p>Follow Us :</p>
-        <img src={Hdata.headerArea.instagramLogo} />
-        <img src={Hdata.headerArea.youtubeLogo} />
-        <img src={Hdata.headerArea.facebookLogo} />
-        <img src={Hdata.headerArea.twitterLogo} />
+
+        <div className="flex ">
+          <p>Follow Us :</p>
+          <img src={Hdata.headerArea.instagramLogo} className="px-2" />
+          <img src={Hdata.headerArea.youtubeLogo} className="px-2" />
+          <img src={Hdata.headerArea.facebookLogo} className="px-2" />
+          <img src={Hdata.headerArea.twitterLogo} className="px-2" />
+        </div>
       </div>
 
       <div className="section2 flex">
