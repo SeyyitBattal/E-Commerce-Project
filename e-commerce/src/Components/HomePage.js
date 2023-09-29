d40748725231e743a4761ab4318c181a116a56fa
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-29 07:47:39 +0300

    Courser added

--- e-commerce/src/Components/HomePage.js
+++ e-commerce/src/Components/HomePage.js
@@ -1,8 +1,42 @@
 import { Hdata } from "../Datas/Hdata";
+import { Carousel } from "@material-tailwind/react";
 
 export const HomePage = () => {
   return (
     <div className="">
+      <Carousel
+        className="rounded-xl bg-red-400"
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                  activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        )}
+      >
+        <div className="flex">
+          <img
+            src={Hdata.bodyArea.sliderPhoto1}
+            alt="image 1"
+            className="h-full w-full object-cover"
+          />
+        </div>
+
+        <div className="flex">
+          <img
+            src={Hdata.bodyArea.sliderPhoto1}
+            alt="image 1"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </Carousel>
+      ***************************************************
       <div className="bg-gradient-to-r from-cyan-500 to-blue-500 object-contain relative">
         <img src={Hdata.bodyArea.sliderPhoto1} className="slider ml-20 " />
         <div class="absolute text-white top-48 left-48">
