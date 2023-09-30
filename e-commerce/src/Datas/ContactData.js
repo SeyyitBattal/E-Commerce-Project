badcc99284c2b3a188f71cf21e1f5caffc4fe125
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-30 21:27:27 +0300

    Contact data updated

--- e-commerce/src/Datas/ContactData.js
+++ e-commerce/src/Datas/ContactData.js
@@ -1,1 +1,27 @@
-export const ContactData = {};
+import familyContact from "../IconsAndPhotos/familyContact.png";
+import twitterLogoContact from "../IconsAndPhotos/twitterLogoContact.svg";
+import facebookLogoContact from "../IconsAndPhotos/facebookLogoContact.svg";
+import instagramLogoContact from "../IconsAndPhotos/instagramLogoContact.svg";
+import linkedinLogoContact from "../IconsAndPhotos/linkedinLogoContact.svg";
+import phoneLogoContact from "../IconsAndPhotos/phoneLogoContact.svg";
+import mapsLogoContact from "../IconsAndPhotos/mapsLogoContact.svg";
+import mailLogoContact from "../IconsAndPhotos/mailLogoContact.svg";
+import ArrowContact from "../IconsAndPhotos/ArrowContact.svg";
+import tryItButton from "../IconsAndPhotos/tryItButton.png";
+
+export const ContactData = {
+  upper: {
+    familyContact,
+    twitterLogoContact,
+    facebookLogoContact,
+    instagramLogoContact,
+    linkedinLogoContact,
+  },
+  lower: {
+    phoneLogoContact,
+    mapsLogoContact,
+    mailLogoContact,
+    ArrowContact,
+    tryItButton,
+  },
+};
