03fb1296ebaa4af40b7541ccf24f15cd3f53c21f
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-30 13:59:06 +0300

    Meet our team updated

--- e-commerce/src/Components/TeamPage.js
+++ e-commerce/src/Components/TeamPage.js
@@ -3,20 +3,22 @@ import { TeamData } from "../Datas/TeamData";
 export const TeamPage = () => {
   return (
     <div>
-      <div>
-        <p>WHAT WE DO</p>
-        <p>Innovation tailored for you</p>
-        <div className="flex">
+      <div className="flex flex-col my-12 ml-80">
+        <p className="text-neutral-500 font-bold ml-96">WHAT WE DO</p>
+        <p className="text-slate-800 font-bold text-7xl my-6">
+          Innovation tailored for you
+        </p>
+        <div className="flex my-10 ml-96">
           <p className="font-bold">Home</p>
           <img src={TeamData.headerArea.HomeToShop} className="mx-3.5" />
           <p className="text-stone-300 font-bold">Team</p>
         </div>
       </div>
-      <div className="flex">
+      <div className="flex ml-auto mr-auto">
         <div>
           <img src={TeamData.headerArea.topImg1} />
         </div>
-        <div className="">
+        <div>
           <div className="flex">
             <img src={TeamData.headerArea.topImg2} />
             <img src={TeamData.headerArea.topImg3} />
@@ -27,28 +29,32 @@ export const TeamPage = () => {
           </div>
         </div>
       </div>
-      <p>Meet Our Team</p>
+      <div className="flex flex-col mx-48">
+        <h1 className=" my-28 text-slate-800 font-bold text-5xl mx-auto">
+          Meet Our Team
+        </h1>
 
-      <div className="flex flex-wrap">
-        {TeamData.bodyArea.teamCardsArray.map((teamCard, index) => (
-          <div
-            className="flex-col shadow-xl text-center mx-4 mb-20"
-            key={index}
-          >
-            <a href="#">
-              <img class="rounded-t-lg" src={teamCard.img} alt="" />
-            </a>
-            <div className="mt-6 mb-9">
-              <p className="font-bold ">{teamCard.username}</p>
-              <p className="text-neutral-500">{teamCard.title}</p>
-              <div className="flex">
-                <img src={teamCard.facebookIcon} />
-                <img src={teamCard.instagramIcon} />
-                <img src={teamCard.twitterIcon} />
+        <div className="flex flex-wrap mx-auto ">
+          {TeamData.bodyArea.teamCardsArray.map((teamCard, index) => (
+            <div
+              className="flex flex-col flex-wrap shadow-xl text-center mx-8 mb-20"
+              key={index}
+            >
+              <a href="#">
+                <img class="rounded-t-lg" src={teamCard.img} alt="" />
+              </a>
+              <div className="mt-6 mb-9">
+                <p className="font-bold ">{teamCard.username}</p>
+                <p className="text-neutral-500">{teamCard.title}</p>
+                <div className="flex">
+                  <img src={teamCard.facebookIcon} />
+                  <img src={teamCard.instagramIcon} />
+                  <img src={teamCard.twitterIcon} />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
 
       <div>
