eaeac30a43be4d052725b639145ffa5faf412c20
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-30 12:09:32 +0300

    Top images added in team page

--- e-commerce/src/Components/TeamPage.js
+++ e-commerce/src/Components/TeamPage.js
@@ -1,8 +1,32 @@
 import { TeamData } from "../Datas/TeamData";
+
 export const TeamPage = () => {
   return (
     <div>
-      <p>Burası Team Page</p>
+      <div>
+        <p>WHAT WE DO</p>
+        <p>Innovation tailored for you</p>
+        <div className="flex">
+          <p className="font-bold">Home</p>
+          <img src={TeamData.headerArea.HomeToShop} className="mx-3.5" />
+          <p className="text-stone-300 font-bold">Team</p>
+        </div>
+      </div>
+      <div className="flex">
+        <div>
+          <img src={TeamData.headerArea.topImg1} />
+        </div>
+        <div className="">
+          <div className="flex">
+            <img src={TeamData.headerArea.topImg2} />
+            <img src={TeamData.headerArea.topImg3} />
+          </div>
+          <div className="flex">
+            <img src={TeamData.headerArea.topImg4} />
+            <img src={TeamData.headerArea.topImg5} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- e-commerce/src/Datas/TeamData.js
+++ e-commerce/src/Datas/TeamData.js
@@ -1,1 +1,19 @@
-export const TeamData = {};
+import HomeToShop from "../IconsAndPhotos/HomeToShop.svg";
+import topImg1 from "../IconsAndPhotos/topImg1.png";
+import topImg2 from "../IconsAndPhotos/topImg2.png";
+import topImg3 from "../IconsAndPhotos/topImg3.png";
+import topImg4 from "../IconsAndPhotos/topImg4.png";
+import topImg5 from "../IconsAndPhotos/topImg5.png";
+
+export const TeamData = {
+  headerArea: {
+    HomeToShop,
+    topImg1,
+    topImg2,
+    topImg3,
+    topImg4,
+    topImg5,
+  },
+  bodyArea: {},
+  footerArea: {},
+};
