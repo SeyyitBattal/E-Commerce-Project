df5f68073fe8e3711c1a55f939c74726c7008175
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-01 13:19:32 +0300

    All sections are created

--- e-commerce/src/Components/AboutPage.js
+++ e-commerce/src/Components/AboutPage.js
@@ -19,6 +19,98 @@ export const AboutPage = () => {
           <img src={AboutData.headerArea.aboutPhoto} />
         </div>
       </div>
+      <div>
+        <label>Problems trying</label>
+        <p>
+          Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
+        </p>
+        <p>
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics
+        </p>
+      </div>
+      <div className="flex">
+        <div>
+          <label>15K</label>
+          <p>Happy Customers</p>
+        </div>
+        <div>
+          <label>150K</label>
+          <p>Monthly Visitors</p>
+        </div>
+
+        <div>
+          <label>15</label>
+          <p>Countries Worldwide</p>
+        </div>
+
+        <div>
+          <label>100+</label>
+          <p>Top Partners</p>
+        </div>
+      </div>
+      <div>BURAYA VİDEO</div>
+      <div>
+        <p className=" my-28 text-slate-800 font-bold text-5xl mx-auto">
+          Meet Our Team
+        </p>
+        <p>
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics
+        </p>
+      </div>
+      <div className="flex flex-wrap mx-48">
+        {AboutData.bodyArea.teamCardsArray.map((teamCard, index) => (
+          <div
+            className="shadow-xl text-center mx-auto mr-14  mb-20"
+            key={index}
+          >
+            <a href="#">
+              <img class="rounded-t-lg" src={teamCard.img} alt="" />
+            </a>
+            <div className="m-8">
+              <p className="font-bold ">{teamCard.username}</p>
+              <p className="text-neutral-500 my-2.5">{teamCard.title}</p>
+              <div className="flex mx-auto justify-center">
+                <img src={teamCard.facebookIcon} />
+                <img src={teamCard.instagramIcon} className="mx-5" />
+                <img src={teamCard.twitterIcon} />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h1>Big Companies Are Here</h1>
+        <p>
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics
+        </p>
+      </div>
+      <div className="flex mx-48 my-24 justify-between">
+        <img src={AboutData.footerArea.brands1} className="object-contain" />
+        <img src={AboutData.footerArea.brands2} className="object-contain" />
+        <img src={AboutData.footerArea.brands3} className="object-contain" />
+        <img src={AboutData.footerArea.brands4} className="object-contain" />
+        <img src={AboutData.footerArea.brands5} className="object-contain" />
+        <img src={AboutData.footerArea.brands6} className="object-contain" />
+      </div>
+      <div className="flex mx-auto justify-center">
+        <div className="text-slate-800 w-96 mt-28 mr-32">
+          <h1 className="font-bold">ABOUT COMPANY</h1>
+          <h1 className="text-6xl font-bold text-slate-800 my-9">ABOUT US</h1>
+          <p className="text-neutral-500 text-xl">
+            We know how large objects will act, but things on a small scale
+          </p>
+          <img
+            src={AboutData.footerArea.aboutWorkWithUsButton}
+            className="mt-9 bg-red-500"
+          />
+        </div>
+        <div>
+          <img src={AboutData.footerArea.aboutWorkWithUs} />
+        </div>
+      </div>
     </div>
   );
 };
--- e-commerce/src/Datas/AboutData.js
+++ e-commerce/src/Datas/AboutData.js
@@ -1,11 +1,64 @@
 import aboutPhoto from "../IconsAndPhotos/aboutPhoto.png";
 import aboutGetQuoteButton from "../IconsAndPhotos/aboutGetQuoteButton.png";
+import aboutWorkWithUs from "../IconsAndPhotos/aboutWorkWithUs.png";
+import aboutWorkWithUsButton from "../IconsAndPhotos/aboutWorkWithUsButton.png";
+import teamMember4 from "../IconsAndPhotos/teamMember4.jpg";
+import teamMember5 from "../IconsAndPhotos/teamMember5.jpg";
+import teamMember6 from "../IconsAndPhotos/teamMember6.jpg";
+import facebookIcon from "../IconsAndPhotos/facebookFooter.svg";
+import instagramIcon from "../IconsAndPhotos/instagramFooter.svg";
+import twitterIcon from "../IconsAndPhotos/twitterFooter.svg";
+import brands1 from "../IconsAndPhotos/fa-brands-1.png";
+import brands2 from "../IconsAndPhotos/fa-brands-2.png";
+import brands3 from "../IconsAndPhotos/fa-brands-3.png";
+import brands4 from "../IconsAndPhotos/fa-brands-4.png";
+import brands5 from "../IconsAndPhotos/fa-brands-5.png";
+import brands6 from "../IconsAndPhotos/fa-brands-6.svg";
 
 export const AboutData = {
   headerArea: {
     aboutPhoto,
     aboutGetQuoteButton,
   },
-  bodyArea: {},
-  footerArea: {},
+  bodyArea: {
+    teamCardsArray: [
+      {
+        id: "1",
+        img: teamMember4,
+        username: "Username",
+        title: "Profession",
+        facebookIcon,
+        instagramIcon,
+        twitterIcon,
+      },
+      {
+        id: "2",
+        img: teamMember5,
+        username: "Username",
+        title: "Profession",
+        facebookIcon,
+        instagramIcon,
+        twitterIcon,
+      },
+      {
+        id: "3",
+        img: teamMember6,
+        username: "Username",
+        title: "Profession",
+        facebookIcon,
+        instagramIcon,
+        twitterIcon,
+      },
+    ],
+  },
+  footerArea: {
+    brands1,
+    brands2,
+    brands3,
+    brands4,
+    brands5,
+    brands6,
+    aboutWorkWithUs,
+    aboutWorkWithUsButton,
+  },
 };
