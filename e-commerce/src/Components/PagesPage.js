6323f34e9a6b1ef108f8c0ca781e5a1f69864097
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-02 21:23:17 +0300

    All datas added in PagesPage

--- e-commerce/src/Components/PagesPage.js
+++ e-commerce/src/Components/PagesPage.js
@@ -3,9 +3,19 @@ import { PagesData } from "../Datas/PagesData";
 export const PagesPage = () => {
   return (
     <div>
-      *******************
-      <p>Burası page</p>
-      ***************************
+      <div className="flex">
+        <p className="font-bold">Home</p>
+        <img src={PagesData.headerArea.HomeToShop} className="mx-3.5" />
+        <p className="text-stone-300 font-bold">Shop</p>
+      </div>
+      <div className="flex mx-48 my-24 justify-between">
+        <img src={PagesData.footerArea.brands1} className="object-contain" />
+        <img src={PagesData.footerArea.brands2} className="object-contain" />
+        <img src={PagesData.footerArea.brands3} className="object-contain" />
+        <img src={PagesData.footerArea.brands4} className="object-contain" />
+        <img src={PagesData.footerArea.brands5} className="object-contain" />
+        <img src={PagesData.footerArea.brands6} className="object-contain" />
+      </div>
     </div>
   );
 };
--- e-commerce/src/Datas/PagesData.js
+++ e-commerce/src/Datas/PagesData.js
@@ -1,5 +1,144 @@
+import HomeToShop from "../IconsAndPhotos/PagesPage/HomeToShop.svg";
+import yellowSeatSlider from "../IconsAndPhotos/PagesPage/yellowSeatSlider.jpg";
+import fullStar from "../IconsAndPhotos/PagesPage/fullStar.svg";
+import emptyStar from "../IconsAndPhotos/PagesPage/emptyStar.svg";
+import blueCircle from "../IconsAndPhotos/PagesPage/blueCircle.svg";
+import yellowCircle from "../IconsAndPhotos/PagesPage/yellowCircle.svg";
+import orangeCircle from "../IconsAndPhotos/PagesPage/orangeCircle.svg";
+import blackCircle from "../IconsAndPhotos/PagesPage/blackCircle.svg";
+import selectOptionsButton from "../IconsAndPhotos/PagesPage/selectOptionsButton.png";
+import heartButton from "../IconsAndPhotos/PagesPage/heartButton.svg";
+import basketButton from "../IconsAndPhotos/PagesPage/basketButton.png";
+import reviewButton from "../IconsAndPhotos/PagesPage/reviewButton.svg";
+import chairPicture from "../IconsAndPhotos/PagesPage/chairPicture.jpg";
+import yellowSmallPicture from "../IconsAndPhotos/PagesPage/yellowSmallPicture.jpg";
+import pinkRoom from "../IconsAndPhotos/PagesPage/pinkRoom.png";
+import arrowInBody from "../IconsAndPhotos/PagesPage/arrowInBody.svg";
+import bestSeller1 from "../IconsAndPhotos/PagesPage/bestSeller1.png";
+import bestSeller2 from "../IconsAndPhotos/PagesPage/bestSeller2.png";
+import bestSeller3 from "../IconsAndPhotos/PagesPage/bestSeller3.png";
+import bestSeller4 from "../IconsAndPhotos/PagesPage/bestSeller4.png";
+
+import brands1 from "../IconsAndPhotos/PagesPage/fa-brands-1.png";
+import brands2 from "../IconsAndPhotos/PagesPage/fa-brands-2.png";
+import brands3 from "../IconsAndPhotos/PagesPage/fa-brands-3.png";
+import brands4 from "../IconsAndPhotos/PagesPage/fa-brands-4.png";
+import brands5 from "../IconsAndPhotos/PagesPage/fa-brands-5.png";
+import brands6 from "../IconsAndPhotos/PagesPage/fa-brands-6.svg";
+
 export const PagesData = {
-  headerArea: {},
-  bodyArea: {},
-  footerArea: {},
+  headerArea: {
+    HomeToShop,
+    yellowSeatSlider,
+    fullStar,
+    emptyStar,
+    blueCircle,
+    yellowCircle,
+    orangeCircle,
+    blackCircle,
+    selectOptionsButton,
+    heartButton,
+    basketButton,
+    reviewButton,
+    chairPicture,
+    yellowSmallPicture,
+  },
+  bodyArea: {
+    pinkRoom,
+    title: "the quick fox jumps over",
+    bigParagraph:
+      "Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.",
+    arrowArray: [
+      {
+        arrowInBody,
+        text: "the quick fox jumps over the lazy dog",
+      },
+      {
+        arrowInBody,
+        text: "the quick fox jumps over the lazy dog",
+      },
+      {
+        arrowInBody,
+        text: "the quick fox jumps over the lazy dog",
+      },
+      {
+        arrowInBody,
+        text: "the quick fox jumps over the lazy dog",
+      },
+    ],
+    bestsellerArray: [
+      {
+        id: 1,
+        img: bestSeller1,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+      {
+        id: 2,
+        img: bestSeller2,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+      {
+        id: 3,
+        img: bestSeller3,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+      {
+        id: 4,
+        img: bestSeller4,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+      {
+        id: 1,
+        img: bestSeller1,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+      {
+        id: 2,
+        img: bestSeller2,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+      {
+        id: 3,
+        img: bestSeller3,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+      {
+        id: 4,
+        img: bestSeller4,
+        tittle: "Graphic Design",
+        descripton: "English department",
+        price1: "$16.48",
+        price2: "$6.48",
+      },
+    ],
+  },
+  footerArea: {
+    brands1,
+    brands2,
+    brands3,
+    brands4,
+    brands5,
+    brands6,
+  },
 };
