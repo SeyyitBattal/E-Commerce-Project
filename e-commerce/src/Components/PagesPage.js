87664a1d0022d5ef26b4917b655344bdac0b5968
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-02 22:02:38 +0300

    All sections added on products page

--- e-commerce/src/Components/PagesPage.js
+++ e-commerce/src/Components/PagesPage.js
@@ -45,6 +45,52 @@ export const PagesPage = () => {
         </div>
       </div>
 
+      <div className="flex">
+        <label>Description</label>
+        <label>Additional Information</label>
+        <label>Reviews</label>
+        <label>( 0 )</label>
+      </div>
+      <hr />
+
+      <div className="flex">
+        <div>
+          <img src={PagesData.bodyArea.pinkRoom} />
+        </div>
+        <div>
+          <label>{PagesData.bodyArea.title}</label>
+          <p>{PagesData.bodyArea.bigParagraph}</p>
+          <p>{PagesData.bodyArea.bigParagraph}</p>
+          <p>{PagesData.bodyArea.bigParagraph}</p>
+        </div>
+        <div>
+          {PagesData.bodyArea.arrowArray.map((eachArticle, index) => (
+            <div className="flex" key={index}>
+              <img src={eachArticle.arrowInBody} />
+              <label>{eachArticle.text}</label>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <label>BESTSELLER PRODUCTS</label>
+        <hr />
+        <div className="flex">
+          {PagesData.bodyArea.bestsellerArray.map((bestCard, index) => (
+            <div className="flex flex-col" key={index}>
+              <img src={bestCard.img} />
+              <label>{bestCard.tittle}</label>
+              <label>{bestCard.descripton}</label>
+              <div className="flex">
+                <label>{bestCard.price1}</label>
+                <label>{bestCard.price2}</label>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div className="flex mx-48 my-24 justify-between">
         <img src={PagesData.footerArea.brands1} className="object-contain" />
         <img src={PagesData.footerArea.brands2} className="object-contain" />
