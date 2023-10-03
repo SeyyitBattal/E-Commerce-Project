cbfd3032027cbe801c366c60f6d49681439aa9a4
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-03 20:12:16 +0300

    Pricing page updated

--- e-commerce/src/Components/PricingPage.js
+++ e-commerce/src/Components/PricingPage.js
@@ -1,1 +1,61 @@
-export const PricingPage = () => {};
+import { PricingData } from "../Datas/PricingData";
+export const PricingPage = () => {
+  return (
+    <div>
+      <div className="flex flex-col my-12  text-center">
+        <p className="text-neutral-500 font-bold flex mx-auto justify-center">
+          PRICING
+        </p>
+        <p className="text-slate-800 font-bold text-7xl my-6">Simple Pricing</p>
+        <div className="flex my-10 mx-auto justify-center">
+          <p className="font-bold">Home</p>
+          <img src={PricingData.headerArea.HomeToShop} className="mx-3.5" />
+          <p className="text-stone-300 font-bold">Pricing</p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center mx-auto my-32">
+        <p className=" text-slate-800 font-bold text-5xl justify-center mx-auto">
+          Pricing
+        </p>
+        <p className="justify-center mx-auto text-neutral-500 my-8 font-bold w-[40rem] text-xl text-center">
+          Problems trying to resolve the conflict between <br /> the two major
+          realms of Classical physics: Newtonian mechanics
+        </p>
+      </div>
+      <p>Trusted By Over 4000 Big Companies</p>
+      <div className="flex mx-48 mt-20 mb-32 justify-between">
+        <img src={PricingData.bodyArea.brands1} className="object-contain" />
+        <img src={PricingData.bodyArea.brands2} className="object-contain" />
+        <img src={PricingData.bodyArea.brands3} className="object-contain" />
+        <img src={PricingData.bodyArea.brands4} className="object-contain" />
+        <img src={PricingData.bodyArea.brands5} className="object-contain" />
+        <img src={PricingData.bodyArea.brands6} className="object-contain" />
+      </div>
+      <div className="flex flex-col justify-center mx-auto my-32">
+        <p className=" text-slate-800 font-bold text-5xl justify-center mx-auto">
+          Start your 14 days free trial
+        </p>
+        <p className="justify-center mx-auto text-neutral-500 my-8 font-bold w-[35rem] text-xl text-center">
+          Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
+          RELIT official consequent.
+        </p>
+        <img
+          src={PricingData.footerArea.tryItButton}
+          className="flex object-none justify-center mx-auto"
+        />
+        <div className="flex justify-center mx-auto mt-8">
+          <img src={PricingData.footerArea.twitterLogoFooter} />
+          <img
+            src={PricingData.footerArea.facebookLogoFooter}
+            className="mx-9"
+          />
+          <img src={PricingData.footerArea.instagramLogoFooter} />
+          <img
+            src={PricingData.footerArea.linkedinLogoFooter}
+            className="ml-9"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- e-commerce/src/Datas/PricingData.js
+++ e-commerce/src/Datas/PricingData.js
@@ -1,5 +1,33 @@
+import HomeToShop from "../IconsAndPhotos/PricingPage/HomeToShop.svg";
+import brands1 from "../IconsAndPhotos/fa-brands-1.png";
+import brands2 from "../IconsAndPhotos/fa-brands-2.png";
+import brands3 from "../IconsAndPhotos/fa-brands-3.png";
+import brands4 from "../IconsAndPhotos/fa-brands-4.png";
+import brands5 from "../IconsAndPhotos/fa-brands-5.png";
+import brands6 from "../IconsAndPhotos/fa-brands-6.svg";
+import tryItButton from "../IconsAndPhotos/tryItButton.png";
+import twitterLogoFooter from "../IconsAndPhotos/twitterLogoFooter.svg";
+import facebookLogoFooter from "../IconsAndPhotos/facebookLogoFooter.svg";
+import instagramLogoFooter from "../IconsAndPhotos/instagramLogoFooter.svg";
+import linkedinLogoFooter from "../IconsAndPhotos/linkedinLogoFooter.svg";
+
 export const PricingData = {
-  headerArea: {},
-  bodyArea: {},
-  footerArea: {},
+  headerArea: {
+    HomeToShop,
+  },
+  bodyArea: {
+    brands1,
+    brands2,
+    brands3,
+    brands4,
+    brands5,
+    brands6,
+  },
+  footerArea: {
+    tryItButton,
+    twitterLogoFooter,
+    facebookLogoFooter,
+    instagramLogoFooter,
+    linkedinLogoFooter,
+  },
 };
