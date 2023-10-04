2db1c5085265267822085b36d306255c8bb4c65c
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-04 19:32:44 +0300

    All pages done without slider

--- e-commerce/src/Components/PricingPage.js
+++ e-commerce/src/Components/PricingPage.js
@@ -2,7 +2,7 @@ import { PricingData } from "../Datas/PricingData";
 export const PricingPage = () => {
   return (
     <div>
-      <div className="flex flex-col my-12  text-center">
+      <div className="flex flex-col my-20  text-center">
         <p className="text-neutral-500 font-bold flex mx-auto justify-center">
           PRICING
         </p>
@@ -13,25 +13,28 @@ export const PricingPage = () => {
           <p className="text-stone-300 font-bold">Pricing</p>
         </div>
       </div>
-      <div className="flex flex-col justify-center mx-auto my-32">
+      <div className="flex flex-col justify-center mx-auto mt-48">
         <p className=" text-slate-800 font-bold text-5xl justify-center mx-auto">
           Pricing
         </p>
-        <p className="justify-center mx-auto text-neutral-500 my-8 font-bold w-[40rem] text-xl text-center">
+        <p className="justify-center mx-auto text-neutral-500 mt-2.5 font-bold w-[40rem] text-xl text-center">
           Problems trying to resolve the conflict between <br /> the two major
           realms of Classical physics: Newtonian mechanics
         </p>
-        <div className="flex mx-auto justify-center">
-          <p className="font-bold text-xl">Monthly</p>
+        <div className="flex mx-auto my-12 justify-center  ">
+          <p className="font-bold text-xl pt-3">Monthly</p>
 
-          <label class="relative inline-flex items-center mb-6 cursor-pointer">
+          <label class="relative inline-flex items-center mt-4 mb-6 mx-4 cursor-pointer ">
             <input type="checkbox" value="" class="sr-only peer" />
 
-            <div class="w-9 h-5 bg-gray-800 peer-focus:outline-none  peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-white dark:border peer-checked:after:translate-x-full peer-checked:after:border-sky-500 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-blue-600 peer-checked:bg-blue-600"></div>
+            <div class="w-9 h-5 bg-gray-800 peer-focus:outline-none  peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-white dark:border peer-checked:after:translate-x-full peer-checked:after:border-sky-500 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-gray-200 after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-blue-600 peer-checked:bg-blue-600"></div>
           </label>
 
-          <p className="font-bold text-xl">Yearly</p>
-          <img src={PricingData.bodyArea.saveButton} />
+          <p className="font-bold text-xl pt-3">Yearly</p>
+          <img
+            className=" object-contain mx-4"
+            src={PricingData.bodyArea.saveButton}
+          />
         </div>
       </div>
       <div className="flex flex-wrap mx-auto justify-center">
@@ -39,8 +42,8 @@ export const PricingPage = () => {
         <img src={PricingData.bodyArea.standardCard} />
         <img src={PricingData.bodyArea.premiumCard} />
       </div>
-      <div>
-        <p className="text-center text-xl">
+      <div className="mt-48">
+        <p className="text-center text-2xl">
           Trusted By Over 4000 Big Companies
         </p>
         <div className="flex flex-wrap mx-48 mt-12 mb-32 justify-between">
@@ -53,7 +56,7 @@ export const PricingPage = () => {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center mx-auto my-20 text-center">
+      <div className="flex flex-col justify-center mx-auto my-48 text-center">
         <div className="my-12">
           <p className=" text-slate-800 font-bold text-5xl justify-center mx-auto">
             Pricing FAQs
@@ -64,17 +67,17 @@ export const PricingPage = () => {
           </p>
         </div>
 
-        <div className="flex mx-48 px-24">
-          <div className="mr-24">
+        <div className="flex mx-48 px-12">
+          <div className="mr-24 ">
             {PricingData.bodyArea.pricingFAQarray.map(
               (everyParagraph, index) => (
-                <div key={index} className="m-6">
+                <div key={index} className="p-6 my-12">
                   <div className="flex">
                     <img src={everyParagraph.arrowBlue} className="mr-5 " />
                     <p className="font-bold">{everyParagraph.title}</p>
                   </div>
 
-                  <p className="text-neutral-500 ml-7 w-[23rem]">
+                  <p className="text-neutral-500 ml-7 w-[25rem]">
                     {everyParagraph.bigParagraph}
                   </p>
                 </div>
@@ -84,7 +87,7 @@ export const PricingPage = () => {
           <div>
             {PricingData.bodyArea.pricingFAQarray.map(
               (everyParagraph, index) => (
-                <div key={index} className="p-6">
+                <div key={index} className="p-6 my-12">
                   <div className="flex">
                     <img src={everyParagraph.arrowBlue} className="mr-5" />
                     <p className="font-bold">{everyParagraph.title}</p>
@@ -98,12 +101,12 @@ export const PricingPage = () => {
             )}
           </div>
         </div>
-        <p className="text-xl text-center text-neutral-500 mt-12 mb-20">
+        <p className="text-2xl text-center text-neutral-500 mt-12 mb-20">
           Haven’t got your answer? Contact our support
         </p>
       </div>
 
-      <div className="flex flex-col justify-center mx-auto my-40">
+      <div className="flex flex-col justify-center mx-auto my-48">
         <p className=" text-slate-800 font-bold text-5xl justify-center mx-auto">
           Start your 14 days free trial
         </p>
