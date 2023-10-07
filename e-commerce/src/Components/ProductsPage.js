b2088101b13444810ae8beeb71131a72558deca7
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-07 21:09:02 +0300

    Product page done

--- e-commerce/src/Components/ProductsPage.js
+++ e-commerce/src/Components/ProductsPage.js
@@ -141,22 +141,8 @@ export const ProductsPage = () => {
           </div>
         ))}
       </div>
-      <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div class="flex flex-1 justify-between sm:hidden">
-          <a
-            href="#"
-            class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Previous
-          </a>
-          <a
-            href="#"
-            class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Next
-          </a>
-        </div>
-        <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+      <div class="flex flex-wrap items-center justify-between border-t border-gray-200 bg-white px-4 py-3 md:mx-0 ">
+        <div className="flex mx-auto justify-center">
           <div className=" mt-12 ml-auto mr-auto">
             <nav
               class="isolate inline-flex -space-x-px rounded-md shadow-sm"
@@ -185,7 +171,7 @@ export const ProductsPage = () => {
               </a>
               <a
                 href="#"
-                class="relative hidden items-center px-4 py-2 text-sm font-semibold text-sky-500 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-sky-500 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
               >
                 3
               </a>
@@ -200,13 +186,31 @@ export const ProductsPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex mx-48 my-24 justify-between">
-        <img src={Pdata.footerArea.brands1} className="object-contain" />
-        <img src={Pdata.footerArea.brands2} className="object-contain" />
-        <img src={Pdata.footerArea.brands3} className="object-contain" />
-        <img src={Pdata.footerArea.brands4} className="object-contain" />
-        <img src={Pdata.footerArea.brands5} className="object-contain" />
-        <img src={Pdata.footerArea.brands6} className="object-contain" />
+      <div className="flex flex-wrap mx-48  my-24 md:mx-48 md:my-24 md:justify-between justify-center">
+        <img
+          src={Pdata.footerArea.brands1}
+          className="object-contain mb-12 md:mb-0"
+        />
+        <img
+          src={Pdata.footerArea.brands2}
+          className="object-contain mb-12 md:mb-0"
+        />
+        <img
+          src={Pdata.footerArea.brands3}
+          className="object-contain mb-12 md:mb-0"
+        />
+        <img
+          src={Pdata.footerArea.brands4}
+          className="object-contain mb-12 md:mb-0"
+        />
+        <img
+          src={Pdata.footerArea.brands5}
+          className="object-contain mb-12 md:mb-0"
+        />
+        <img
+          src={Pdata.footerArea.brands6}
+          className="object-contain mb-12 md:mb-0 "
+        />
       </div>
     </div>
   );
