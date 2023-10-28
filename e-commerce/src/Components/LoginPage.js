5af332700c110f91f3549dba5f6d2faa34cbb097
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-28 11:41:10 +0300

    Login page updated

--- e-commerce/src/Components/LoginPage.js
+++ e-commerce/src/Components/LoginPage.js
@@ -157,66 +157,68 @@ export const LoginPage = () => {
 
   return (
     <div className="flex mx-auto justify-center my-20">
-      <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg">
-        <div className="flex flex-wrap -mx-3 mb-6"></div>
-        <div className="w-full md:w-1/2 px-3 -mx-3 mb-6">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-last-name"
-          >
-            e-mail
-          </label>
-          <input
-            className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${
-              errors.email ? "border-red-500" : "border-gray-200"
-            } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
-            type="text"
-            placeholder="Email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^\S+@\S+$/i,
-                message: "Please enter a valid email address",
-              },
-            })}
-          />
-          {errors.email && (
-            <p className="text-red-500 text-xs italic">
-              {errors.email.message}
-            </p>
-          )}
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
+      <form onSubmit={handleSubmit(onSubmit)} className="">
+        <div className="mx-auto flex flex-wrap justify-center ">
+          <div className="w-full px-6 -mx-3 mb-6 ">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-password"
+              htmlFor="grid-last-name"
             >
-              Password
+              e-mail
             </label>
             <input
               className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${
-                errors.password ? "border-red-500" : "border-gray-200"
-              } rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
-              id="grid-password"
-              type="password"
-              placeholder="Password"
-              {...register("password", {
-                required: "Password is required",
+                errors.email ? "border-red-500" : "border-gray-200"
+              } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+              type="text"
+              placeholder="Email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Please enter a valid email address",
+                },
               })}
             />
-            {errors.password && (
+            {errors.email && (
               <p className="text-red-500 text-xs italic">
-                {errors.password.message}
+                {errors.email.message}
               </p>
             )}
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6 w-full  px-3   ">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Password
+              </label>
+              <input
+                className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${
+                  errors.password ? "border-red-500" : "border-gray-200"
+                } rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+                id="grid-password"
+                type="password"
+                placeholder="Password"
+                {...register("password", {
+                  required: "Password is required",
+                })}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-xs italic">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
           </div>
         </div>
 
         <button
           type="submit"
           disabled={loading}
-          className="flex mx-auto justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-3 text-xl px-12 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-20"
+          className="flex mx-auto justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-3 text-xl px-12 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-8"
         >
           {loading ? "Loading..." : "Submit"}
         </button>
